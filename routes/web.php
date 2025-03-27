19a837e62cb59ba259f592efd59bfb0175ae3bd7
Fauzi Hizbullah <fauzihiz@gmail.com>
2025-03-28 01:56:05 +0700

    exercise 09

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::view('/second', 'second')->name('second');
 Route::view('contact', 'contact')->name('contact');
